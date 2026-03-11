4658ef7493a633f79540f25baf02c146132c63f6
TatiParra21 <tatiparras21@gmail.com>
2026-03-11 10:19:53 -0500

    fixing login issiu

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,20 +1,22 @@
 import { supabase } from "../supabaseClient"
-import { useNavigate } from "react-router-dom"
 import { authStateStore } from "../store/projectStore"
 export const LoginButton =()=>{
   const  setAuthError = authStateStore(state=>state.setAuthError)
-        const navigate = useNavigate()
+    
         const signInWithGoogle =async()=>{          
              const {error} = await supabase.auth.signInWithOAuth({
             provider:"google",
-              options: { redirectTo: window.location.origin  }           
+              options: { redirectTo: window.location.origin,queryParams: {
+          prompt: "select_account",
+            },  
+            }           
         })
         if(error){
             setAuthError(`Google sign in error: ${error.message}`, )
         }else{ 
             setAuthError("redirecting . google login")
         }  
-       navigate("/")       
+        
     }
     return(
         <>
